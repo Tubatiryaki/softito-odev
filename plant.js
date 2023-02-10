a26348a30b007d8aea7a595b882426c21809e4fa
Tubatiryaki <tubatiryaki25@gmail.com>
2023-02-10 22:09:42 +0300

    softito ödev

---
+++ plant.js
@@ -0,0 +1,29 @@
+
+function hamMenu(){
+   var icon= document.getElementById("myMenu");
+   if(icon.style.display==="none"){
+    icon.style.display === "block";
+   }else{
+    icon.style.display === "none";
+   }
+}
+
+function tarihSaat(){
+    var date=new Date().toLocaleString('tr-TR');
+    document.querySelector(".saat").innerHTML=date;
+}
+
+setInterval(tarihSaat, 1000);
+
+
+
+function loginHide(){
+var change=document.getElementById("myLogin");
+if(change.style.display === "none")
+{
+    change.style.display="block";
+}else{
+    change.style.display="none";
+}
+}
+
